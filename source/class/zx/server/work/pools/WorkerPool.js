b6842530e5f839552f6b51979005dbdffbb32194
Patryk Milinowski <pmilinowski@vmn.digital>
2025-08-12 16:17:07 +0100

    minor fixes

--- source/class/zx/server/work/pools/WorkerPool.js
+++ source/class/zx/server/work/pools/WorkerPool.js
@@ -302,6 +302,7 @@ qx.Class.define("zx.server.work.pools.WorkerPool", {
      * Apply for `poolConfig` property
      */
     _applyPoolConfig(value, oldValue) {
+      if (!value) return;
       let pool = this.getQxObject("pool");
       ["minSize", "maxSize", "timeout", "pollInterval"].forEach(prop => {
         let upname = qx.lang.String.firstUp(prop);
