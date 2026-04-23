ab4cd841414cad1ffc9fd6c930d7adad91fd584f
Patryk Milinowski <pmilinowski@vmn.digital>
2026-04-23 11:56:25 +0100

    fix: not possible to keep getUserFromEmail mutexed when overriding getUserFromEmail

--- source/class/zx/server/auth/UserDiscovery.js
+++ source/class/zx/server/auth/UserDiscovery.js
@@ -30,44 +30,58 @@ qx.Class.define("zx.server.auth.UserDiscovery", {
 
     /**
      * Gets a user from the email address; returns null unless `create` is true, in which case
-     * the user will be created if it does not already exist
+     * the user will be created if it does not already exist.
+     *
+     * Do not override this method; override _getUserFromEmailImpl instead.
+     *
+     * This method calls _getUserFromEmailImpl which does the actual work of finding the user but it mutexes calls.
      *
      * @param {String} email
      * @param {Boolean?} create
-     * @return {zx.server.auth.User}
+     * @return {Promise<zx.server.auth.User>}
      */
     async getUserFromEmail(email, create) {
-      const getUserFromEmailImpl = async (email, create) => {
-        let server = zx.server.Standalone.getInstance();
-        let clazz = zx.server.auth.User.getUserClass();
-        let user = await server.findOneObjectByType(clazz, {
-          username: email.toLowerCase()
-        });
-
-        if (!user && create) {
-          user = new clazz().set({
-            username: email,
-            fullName: ""
-          });
-
-          await user.save();
-        }
-        return user;
-      };
-
       if (this.__promise) {
         return this.__promise.then(() => this.getUserFromEmail(email, create));
       }
 
-      let promise = getUserFromEmailImpl(email, create);
+      let promise = this._getUserFromEmailImpl(email, create);
       this.__promise = promise;
       promise.then(user => {
-        if (promise === this.__promise) {
-          this.__promise = null;
-        }
+        this.__promise = null;
         return user;
       });
       return await promise;
+    },
+
+    /**
+     * Gets a user from the email address; returns null unless `create` is true, in which case
+     * the user will be created if it does not already exist.
+     * The implementation does not need to worry about any race conditions;
+     * calls to this method are mutexed.
+     *
+     * Can be overridden.
+     *
+     * @param {String} email
+     * @param {Boolean} create
+     * @returns {Promise<zx.server.auth.User>}
+     */
+    async _getUserFromEmailImpl(email, create) {
+      let server = zx.server.Standalone.getInstance();
+      let clazz = zx.server.auth.User.getUserClass();
+      let user = await server.findOneObjectByType(clazz, {
+        username: email.toLowerCase()
+      });
+
+      if (!user && create) {
+        user = new clazz().set({
+          username: email,
+          fullName: ""
+        });
+
+        await user.save();
+      }
+      return user;
     }
   }
 });
